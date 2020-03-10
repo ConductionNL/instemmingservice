a75c5eb6b54ddaec65446251791e2bc67b549240
Ruben van der Linde <ruben@conduction.nl>
2020-03-10 10:46:11 +0100

    Added filters and audit trail

--- api/src/Entity/Assent.php
+++ api/src/Entity/Assent.php
@@ -2,9 +2,13 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -18,9 +22,36 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ApiResource(
  *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
- *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true}
+ *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
+ *     itemOperations={
+ *          "get",
+ *          "put",
+ *          "delete",
+ *          "get_change_logs"={
+ *              "path"="/assents/{id}/change_log",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Changelogs",
+ *                  "description"="Gets al the change logs for this resource"
+ *              }
+ *          },
+ *          "get_audit_trail"={
+ *              "path"="/assents/{id}/audit_trial",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Audittrail",
+ *                  "description"="Gets the audit trail for this resource"
+ *              }
+ *          }
+ *     },
  * )
  * @ORM\Entity(repositoryClass="App\Repository\AssentRepository")
+ * @Gedmo\Loggable(logEntryClass="App\Entity\ChangeLog")
+ * 
+ * @ApiFilter(BooleanFilter::class)
+ * @ApiFilter(OrderFilter::class)
+ * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
+ * @ApiFilter(SearchFilter::class)
  */
 class Assent
 {
@@ -48,9 +79,10 @@ class Assent
 
     /**
      * @var string The name of this assend is displayed as a title to end users and should make clear what they arre assending to
-     *
      * @example My Assent
      *
+     * 
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank
@@ -62,9 +94,9 @@ class Assent
 
     /**
      * @var string The description of this assend is displayed to end users as aditional information and should make clear what they arre assending to
-     *
      * @example This is the best assent ever
      *
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="text", nullable=true)
      */
@@ -72,9 +104,9 @@ class Assent
 
     /**
      * @var string The request that this assent applies to
-     *
      * @example https://www.example.org/requests/1
      *
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      * @ApiFilter(SearchFilter::class, strategy="exact")
@@ -86,9 +118,9 @@ class Assent
 
     /**
      * @var string The property of a request that this assent applies to e.g. parner in meldingvoorgenomenhuwelijk
-     *
      * @example https://www.example.org/people/1
      *
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      * @ApiFilter(SearchFilter::class, strategy="exact")
@@ -100,9 +132,10 @@ class Assent
 
     /**
      * @var string The process that this assent originated from
-     *
      * @example https://www.example.org/processes/1
      *
+     * 
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      * @ApiFilter(SearchFilter::class, strategy="exact")
@@ -114,9 +147,10 @@ class Assent
 
     /**
      * @var string The contact that this assent applies to
-     *
      * @example https://www.example.org/contacts/1
      *
+     * 
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      * @ApiFilter(SearchFilter::class, strategy="exact")
@@ -128,9 +162,9 @@ class Assent
 
     /**
      * @var string The person that this assent applies to
-     *
      * @example https://www.example.org/people/2
-     *
+     * 
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="string", length=255, nullable=true)
      * @ApiFilter(SearchFilter::class, strategy="exact")
@@ -142,9 +176,9 @@ class Assent
 
     /**
      * @var string The status of this assent e.g. requested, granted, declined
+     * example requested
      *
-     * @example requested
-     *
+     * @Gedmo\Versioned
      * @Assert\Choice({"requested", "granted", "submitted", "declined"})
      * @Assert\Length(
      *      max = 255
@@ -157,6 +191,7 @@ class Assent
     /**
      * @var string requester The organisation (RSIN) or person (BSN) that is responsible for making this assent
      *
+     * @Gedmo\Versioned
      * @Groups({"read","write"})
      * @ORM\Column(type="string", length=255)
      * @ApiFilter(SearchFilter::class, strategy="exact")
@@ -166,6 +201,24 @@ class Assent
      * )
      */
     private $requester;
+    
+    /**
+     * @var Datetime $dateCreated The moment this resource was created
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateCreated;
+    
+    /**
+     * @var Datetime $dateModified  The moment this resource last Modified
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateModified;
 
     public function getId()
     {
@@ -290,5 +343,29 @@ class Assent
         $this->requester = $requester;
 
         return $this;
+    }
+    
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+    	return $this->dateCreated;
+    }
+    
+    public function setDateCreated(\DateTimeInterface $dateCreated): self
+    {
+    	$this->dateCreated= $dateCreated;
+    	
+    	return $this;
+    }
+    
+    public function getDateModified(): ?\DateTimeInterface
+    {
+    	return $this->dateModified;
+    }
+    
+    public function setDateModified(\DateTimeInterface $dateModified): self
+    {
+    	$this->dateModified = $dateModified;
+    	
+    	return $this;
     }
 }
