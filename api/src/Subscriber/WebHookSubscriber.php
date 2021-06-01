ded5c9c0794bffce6c8d5a0b1c706a85a5d91a3a
Gino <ginokok1996@hotmail.com>
2021-06-01 16:36:33 +0200

    update

--- api/src/Subscriber/WebHookSubscriber.php
+++ api/src/Subscriber/WebHookSubscriber.php
@@ -5,26 +5,17 @@ namespace App\Subscriber;
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\WebHook;
 use App\Service\AssentService;
-use Conduction\CommonGroundBundle\Service\CommonGroundService;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class WebHookSubscriber implements EventSubscriberInterface
 {
-    private $params;
     private $trouwService;
-    private $serializer;
-    private $commonGroundService;
 
-    public function __construct(ParameterBagInterface $params, AssentService $trouwService, CommongroundService $commonGroundService, SerializerInterface $serializer)
+    public function __construct(AssentService $trouwService)
     {
-        $this->params = $params;
         $this->trouwService = $trouwService;
-        $this->commonGroundService = $commonGroundService;
-        $this->serializer = $serializer;
     }
 
     public static function getSubscribedEvents()
